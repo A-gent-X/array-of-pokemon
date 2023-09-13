5cd513b0e4df351c290c94b263fe0c41ac3ab605
Xavier Brown <xavieraustin1990@gmail>
2023-09-13 14:28:22 -0400

    initial commit: attack compared

---
+++ arrays.js
@@ -0,0 +1,15 @@
+/*
+    In this file, we'll be continuing our story
+    with Jon being allowed to flee his fight with Jamie.
+*/
+
+let jonSnowAttack = 25;
+let jamieLannisterAttack = 35;
+ 
+if (jonSnowAttack > jamieLannisterAttack){
+    console.log("Jon Snow has better attack than Jamie Lannister");
+
+} else{
+    console.log("Jamie Lannister has better attack than Jon Snow");
+}
+
---
+++ varsAndConditionals.js
@@ -0,0 +1,16 @@
+/*
+    In this file, we'll be using variables, conditionals,
+    and loops to play out a fight between Jon Snow and
+    Jamie Lannister.
+*/ 
+
+
+let jonSnowAttack = 25;
+let jamieLannisterAttack = 35;
+ 
+if (jonSnowAttack > jamieLannisterAttack){
+    console.log("Jon Snow has better attack than Jamie Lannister");
+
+} else{
+    console.log("Jamie Lannister has better attack than Jon Snow");
+}
