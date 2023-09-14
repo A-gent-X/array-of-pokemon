a5444e484599b6ea4f008ad7482acc0fcdc79460
Xavier Brown <xavieraustin1990@gmail>
2023-09-14 13:41:43 -0400

    pokemon

--- arrays.js
+++ arrays.js
@@ -3,13 +3,54 @@
     with Jon being allowed to flee his fight with Jamie.
 */
 
-let jonSnowAttack = 25;
-let jamieLannisterAttack = 35;
- 
-if (jonSnowAttack > jamieLannisterAttack){
-    console.log("Jon Snow has better attack than Jamie Lannister");
+let backpack = [ 
+    "poke ball", "poke ball",
+"potion", "beef jerky", "running shoes"
+]
+   
+let pokemon = []
 
-} else{
-    console.log("Jamie Lannister has better attack than Jon Snow");
-}
+backpack.push("oreos")
+backpack.push("oreos")
 
+console.log("after buying oreos, this is our backpack:")
+console.log(backpack)
+
+backpack.push("pokedex")
+pokemon.push("pikachu")
+
+console.log("after ,eetomg professor oak, our backpack and pokemon are:")
+console.log(backpack)
+console.log(pokemon)
+
+
+for(let i = 0; i < 4; i++)
+pokemon.push("pidgey")
+console.log("after the pidgeys, our pokemon are now:")
+console.log(pokemon)
+
+backpack.splice(3, 4);
+console.log("doug ate his food and gave his shoes away")
+console.log(backpack)
+
+let newPokemon = "Charizard"
+
+pokemon.push(newPokemon)
+
+console.log("after catching charizard, our pokemon is now:")
+console.log(pokemon)
+
+console.log("here is my pikachu!")
+let cutestPokemon = pokemon[0]
+console.log(cutestPokemon)
+
+let clonedPidgeys = pokemon.slice(1, 5)
+console.log("here are your cloned pidgeys")
+console.log(clonedPidgeys)
+
+console.log("checking to make sure my pokemon are all still here:")
+console.log(pokemon)
+
+backpack.unshift("jynx statue")
+console.log("backpack after adding the statue:")
+console.log(backpack)
--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -14,3 +14,47 @@ if (jonSnowAttack > jamieLannisterAttack){
 } else{
     console.log("Jamie Lannister has better attack than Jon Snow");
 }
+
+let jonSnowHealth = 100
+let jonSnowDefense = 0
+
+if (jonSnowHealth <= jamieLannisterAttack) {
+    console.log("Jon Snow has been slain.")
+} else {
+    jonSnowHealth = jonSnowHealth - jamieLannisterAttack
+    console.log(`Jon Snow's health is down to ${jonSnowHealth}`)
+}
+
+jonSnowDefense += 25
+
+//second attack from Jamie
+if (jonSnowHealth <= jamieLannisterAttack - jonSnowDefense){
+    console.log("Jon Snow is dead")
+} else {
+    jonSnowHealth -= jamieLannisterAttack - jonSnowDefense
+    console.log(`Jon Snow's health is down to ${jonSnowHealth}`)
+}
+
+let isDead = false
+//Jamie uses a gatling gun at Jon  1 times as he runs into the forest
+//there is a 50% chance, per gun  usage, that Jon is dead
+for (let i = 1; i <= 3; i++) {
+    isDead = Math.random() > .5
+    if (isDead === true) {
+        console.log(`John has died after ${i} rounds`)
+        break
+    }
+}
+
+if (isDead === false) {
+    console.log(`It is a miracle! John is alive after the bombardment.`)
+}
+
+//Jamie wants Jon dead, and ensures it will happen
+//he pursues Jon into the woods to use his fists
+while (jonSnowHealth > 0) {
+    console.log("Jamie punches Jon")
+    jonSnowHealth -= 5
+}
+
+console.log("Okay, Jon did not make it, sorry.")
